7750445d54246e23b6f42147d17a533a59de15d1
Shaun <shaun@thenetninja.co.uk>
2022-06-15 21:27:03 +0100

    lesson-7

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,18 @@ const createToken = (_id) => {
 
 // login a user
 const loginUser = async (req, res) => {
-  res.json({mssg: 'login user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 // signup a user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -44,4 +44,24 @@ userSchema.statics.signup = async function(email, password) {
   return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
+
 module.exports = mongoose.model('User', userSchema)
