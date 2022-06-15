1b309f19d1fa60a514f1fea71d506e8dd089bdc9
Shaun <shaun@thenetninja.co.uk>
2022-06-15 19:22:31 +0100

    lesson-3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,15 @@ const loginUser = async (req, res) => {
 
 // signup a user
 const signupUser = async (req, res) => {
-  res.json({mssg: 'signup user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.signup(email, password)
+
+    res.status(200).json({email, user})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 module.exports = { signupUser, loginUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +14,22 @@ const userSchema = new Schema({
     required: true
   }
 })
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw Error('Email already in use')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user
+}
 
 module.exports = mongoose.model('User', userSchema)
