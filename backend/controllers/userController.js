76de3f99793e6b4de954921c88b574c870abcd7a
Shaun <shaun@thenetninja.co.uk>
2022-06-15 21:22:24 +0100

    lesson-6

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // login a user
 const loginUser = async (req, res) => {
@@ -12,7 +17,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
 
-    res.status(200).json({email, user})
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
