11a0b3e6d4b5499dcb0c71f74aa7f02cca0d1e6c
Shaun <shaun@thenetninja.co.uk>
2022-06-16 10:26:05 +0100

    lesson-14

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
+
+const requireAuth = async (req, res, next) => {
+  // verify user is authenticated
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res.status(401).json({error: 'Authorization token required'})
+  }
+
+  const token = authorization.split(' ')[1]
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET)
+
+    req.user = await User.findOne({ _id }).select('_id')
+    next()
+
+  } catch (error) {
+    console.log(error)
+    res.status(401).json({error: 'Request is not authorized'})
+  }
+}
+
+module.exports = requireAuth
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,8 +6,12 @@ const {
   deleteWorkout,
   updateWorkout
 } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
 
 // GET all workouts
 router.get('/', getWorkouts)
