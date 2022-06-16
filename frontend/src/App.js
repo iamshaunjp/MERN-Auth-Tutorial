8db90d8d69caa474a9a6f62431e6996fd9b3489c
Shaun <shaun@thenetninja.co.uk>
2022-06-16 10:40:52 +0100

    lesson-16

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
 import Home from './pages/Home'
@@ -7,6 +8,8 @@ import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -14,16 +17,16 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
-              path="/"
-              element={<Home />}
+              path="/" 
+              element={user ? <Home /> : <Navigate to="/login" />} 
             />
             <Route 
               path="/login" 
-              element={<Login />} 
+              element={!user ? <Login /> : <Navigate to="/" />} 
             />
             <Route 
               path="/signup" 
-              element={<Signup />} 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
             />
           </Routes>
         </div>
